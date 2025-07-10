59d568578397537adae0c40ef56a9c8cf685c4fc
moranmoriarty5 <moranmoriarty22@gmail.com>
2025-07-10 14:26:37 +0700

    soal 3 add api patch

--- basic-server/src/ilham_221058016.ts
+++ basic-server/src/ilham_221058016.ts
@@ -6,7 +6,7 @@ const port = 8016; // Port untuk server Anda
 
 // Middleware untuk parsing JSON body
 app.use(express.json());
-
+let counter = 0;
 
 // Contoh endpoint GET
 app.get('/', (req, res) => {
@@ -24,6 +24,16 @@ app.post('/api/data', (req, res) => {
  }
 });
 
+app.patch("/counter", (req, res) => {
+  const { value } = req.body;
+
+  if (typeof value !== "number") {
+    return res.status(400).json({ error: "Value harus berupa angka" });
+  }
+
+  counter += value; // Tambahkan ke counter
+  res.json({ counter }); // Kirim nilai counter terbaru
+});
 
 // Menjalankan server
 app.listen(port, () => {
