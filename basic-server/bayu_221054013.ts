466b3b0d53c1fbf851092b1afc74702f6ea906ee
Saisyou404 <bayuyue111@gmail.com>
2025-07-10 14:20:29 +0700

    Membuat API PATCH bayu_221054013

--- basic-server/bayu_221054013.ts
+++ basic-server/bayu_221054013.ts
@@ -1,10 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const PORT = 4013;
 
-app.get('/', (req, res) => {
-  res.send('Halo, saya Bayu (221054013)!');
+app.use(express.json());
+
+let counter = 0;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Halo dari Bayu - 221054013');
+});
+
+app.patch('/counter', (req: Request, res: Response) => {
+  const { nilai } = req.body;
+
+  if (typeof nilai !== 'number') {
+    return res.status(400).json({ error: 'Request body harus berisi "nilai" bertipe number' });
+  }
+
+  counter += nilai;
+  res.json({ total: counter });
 });
 
 app.listen(PORT, () => {
