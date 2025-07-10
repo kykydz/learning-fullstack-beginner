e7b2beb863058ccf472a86c18cf1b26be07c9255
cindyy01 <cindyfatika1603@gmail.com>
2025-07-10 14:06:12 +0700

    feat: soal 3 add api patch

--- responsi/221054010/Basic-server/cindyfatika_221054010.ts
+++ responsi/221054010/Basic-server/cindyfatika_221054010.ts
@@ -1,10 +1,30 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json()); // biar bisa baca JSON
+
 const PORT = 4010;
 
+let counter = 0; // nilai awal counter
+
 app.get('/', (_req, res) => {
-  res.send('Halo! Ini server Cindy Fatika (NIM: 221054010) berjalan di port ' + PORT);
+  res.send(`Halo! Ini server Cindy Fatika (NIM: 221054010) berjalan di port ${PORT}`);
+});
+
+// PATCH /counter
+app.patch('/counter', (req, res) => {
+  const { increment } = req.body;
+
+  if (typeof increment !== 'number') {
+    return res.status(400).json({ message: 'Body harus berisi angka pada field increment' });
+  }
+
+  counter += increment;
+
+  res.json({
+    message: 'Counter berhasil ditambahkan',
+    counter: counter
+  });
 });
 
 app.listen(PORT, () => {
